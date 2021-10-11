b0f54b9e18595cfdd1a97362df4fa735f666246e
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-11 11:29:53 +0100

    Passed all the first tests

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,19 +1,32 @@
 import Player from "../player";
 import Square from "../square";
 import Piece from "./piece";
-
 export default class Pawn extends Piece {
   constructor(player) {
     super(player);
   }
 
   getAvailableMoves(board) {
-    let location = board.findPiece(this);
-    console.log(this.player)
+    let currentSquare = board.findPiece(this);
+    console.log(this.player);
     if (this.player === Player.WHITE) {
-      return [Square.at(location.row + 1, location.col)];
-    } else {
-      return [Square.at(location.row - 1, location.col)];
+      if (this.MoveCounter < 1) {
+        return [
+          Square.at(currentSquare.row + 1, currentSquare.col),
+          Square.at(currentSquare.row + 2, currentSquare.col),
+        ];
+      }
+      return [Square.at(currentSquare.row + 1, currentSquare.col)];
+    }
+    if (this.player === Player.BLACK) {
+      if (this.MoveCounter < 1) {
+        return [
+          Square.at(currentSquare.row -1, currentSquare.col),
+          Square.at(currentSquare.row - 2, currentSquare.col),
+        ];
+      } else {
+        return [Square.at(currentSquare.row - 1, currentSquare.col)];
+      }
     }
   }
 }
--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,14 +1,18 @@
 export default class Piece {
-    constructor(player) {
-        this.player = player;
-    }
+  constructor(player) {
+    this.player = player;
+    this.MoveCounter = 0;
+  }
 
-    getAvailableMoves(board) {
-        throw new Error('This method must be implemented, and return a list of available moves');
-    }
+  getAvailableMoves(board) {
+    throw new Error(
+      "This method must be implemented, and return a list of available moves"
+    );
+  }
 
-    moveTo(board, newSquare) {
-        const currentSquare = board.findPiece(this);
-        board.movePiece(currentSquare, newSquare);
-    }
+  moveTo(board, newSquare) {
+    const currentSquare = board.findPiece(this);
+    board.movePiece(currentSquare, newSquare);
+    this.MoveCounter++;
+  }
 }
